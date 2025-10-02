031dbc04ecdc2d55037442501d9fe2e94df7c3ff
Vidhyang Jain <vidhyang387@gmail.com>
2025-10-03 00:58:17 +0530

    assignment-1

--- calculator.c
+++ calculator.c
@@ -2,6 +2,7 @@
 #include <conio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define MAX 100
 
@@ -9,29 +10,35 @@ int values[MAX];
 char ops[MAX];
 int valTop = -1, opTop = -1;
 
-void pushVal(int x) { 
+//so actually i am using here some stack logic in which opTop is the pointer to stack of 
+//operations stack of operations generally contains all the operations and valTop is the 
+//pointer to the top of values and it contains values in which to perform so as the operation
+//comes we will check if any operations is there whose priority is more than first we perform 
+//them and save the result again in the stack of values as this we go further
+
+void pushVal(int x) {   //to push the value in values array
     values[++valTop] = x; 
 }
 
-int popVal() { 
+int popVal() { //to pop the value in values array
     return values[valTop--]; 
 }
 
-void pushOp(char x) { 
+void pushOp(char x) { //to push the operations in ops array
     ops[++opTop] = x; 
 }
 
-char popOp() { 
+char popOp() { //to pop the operation in ops array
     return ops[opTop--]; 
 }
 
-int precedence(char op) {
+int precedence(char op) {  //selecting priority
     if (op == '+' || op == '-') return 1;
     if (op == '*' || op == '/') return 2;
     return 0;
 }
 
-int applyOp(int a, int b, char op, int *errorFlag) {
+int applyOp(int a, int b, char op, int *errorFlag) {  //to apply operation btwn two numbers
     switch (op) {
         case '+': return a + b;
         case '-': return a - b;
@@ -51,7 +58,7 @@ int evaluateExpression(const char *expr, int *errorFlag) {
     for (i = 0; i < strlen(expr); i++) {
         if (expr[i] == ' ') continue;
 
-        if (isdigit(expr[i])) {
+        if (isdigit(expr[i])) {  //if digit store complete digit in stack
             int val = 0;
             while (i < strlen(expr) && isdigit(expr[i])) {
                 val = (val * 10) + (expr[i] - '0');
@@ -61,7 +68,7 @@ int evaluateExpression(const char *expr, int *errorFlag) {
             i--;
         }
         else if (expr[i] == '+' || expr[i] == '-' ||
-                 expr[i] == '*' || expr[i] == '/') {
+                 expr[i] == '*' || expr[i] == '/') {  //if more priority exist then first complete opr then push
             while (opTop != -1 && precedence(ops[opTop]) >= precedence(expr[i])) {
                 int val2 = popVal();
                 int val1 = popVal();
@@ -78,7 +85,7 @@ int evaluateExpression(const char *expr, int *errorFlag) {
         }
     }
 
-    while (opTop != -1) {
+    while (opTop != -1) {   //do with remaining
         int val2 = popVal();
         int val1 = popVal();
         char op = popOp();
