03bcdc64b25adff981285c193992915ec559067e
Vidhyang <vidhyang387@gmail.com>
2025-10-23 13:55:24 +0530

    changed format

--- calculator.c
+++ calculator.c
@@ -9,71 +9,90 @@ int operandStack[MAX_STACK_SIZE];
 char operatorStack[MAX_STACK_SIZE];
 int operandTop = -1, operatorTop = -1;
 
-void pushOperand(int value) { 
-    operandStack[++operandTop] = value; 
+void pushOperand(int value)
+{
+    operandStack[++operandTop] = value;
 }
 
-int popOperand() { 
-    return operandStack[operandTop--]; 
+int popOperand()
+{
+    return operandStack[operandTop--];
 }
 
-void pushOperator(char op) { 
-    operatorStack[++operatorTop] = op; 
+void pushOperator(char op)
+{
+    operatorStack[++operatorTop] = op;
 }
 
-char popOperator() { 
-    return operatorStack[operatorTop--]; 
+char popOperator()
+{
+    return operatorStack[operatorTop--];
 }
 
-int getPrecedence(char operatorSymbol) { 
+int getPrecedence(char operatorSymbol)
+{
     int value = 0;
-    if (operatorSymbol == '+' || operatorSymbol == '-'){ 
+    if (operatorSymbol == '+' || operatorSymbol == '-')
+    {
         value = 1;
     }
-    else if (operatorSymbol == '*' || operatorSymbol == '/'){ 
+    else if (operatorSymbol == '*' || operatorSymbol == '/')
+    {
         value = 2;
     }
     return value;
 }
 
-int performOperation(int leftOperand, int rightOperand, char operatorSymbol, int *errorFlag) {  
+int performOperation(int leftOperand, int rightOperand, char operatorSymbol, int *errorFlag)
+{
     int result = 0;
-    switch (operatorSymbol) {
-        case '+': {
-            result = leftOperand + rightOperand; 
-            break;
+    switch (operatorSymbol)
+    {
+    case '+':
+    {
+        result = leftOperand + rightOperand;
+        break;
+    }
+    case '-':
+    {
+        result = leftOperand - rightOperand;
+        break;
+    }
+    case '*':
+    {
+        result = leftOperand * rightOperand;
+        break;
+    }
+    case '/':
+    {
+        if (rightOperand == 0)
+        {
+            *errorFlag = 1;
+            return 0;
         }
-        case '-': {
-            result = leftOperand - rightOperand; 
-            break;
-        }
-        case '*': {
-            result = leftOperand * rightOperand; 
-            break;
-        }
-        case '/': {
-            if (rightOperand == 0) {
-                *errorFlag = 1;
-                return 0;
-            }
-            result = leftOperand / rightOperand;
-            break;
-        }
+        result = leftOperand / rightOperand;
+        break;
+    }
     }
     return result;
 }
 
-int evaluateExpression(const char *expression, int *errorFlag) {
+int evaluateExpression(const char *expression, int *errorFlag)
+{
     int index;
     int expressionLength = strlen(expression);
     int finalResult = 0;
 
-    for (index = 0; index < expressionLength; index++) {
-        if (expression[index] == ' ') continue;
+    for (index = 0; index < expressionLength; index++)
+    {
+        if (expression[index] == ' ')
+            continue;
 
-        if (isdigit(expression[index])) { 
+        if (isdigit(expression[index]))
+        {
             int value = 0;
-            while (index < expressionLength && isdigit(expression[index])) {
+            while (index < expressionLength && isdigit(expression[index]))
+            {
                 value = (value * 10) + (expression[index] - '0');
                 index++;
             }
@@ -81,45 +100,54 @@ int evaluateExpression(const char *expression, int *errorFlag) {
             index--;
         }
         else if (expression[index] == '+' || expression[index] == '-' ||
-                 expression[index] == '*' || expression[index] == '/') {  
-            while (operatorTop != -1 && 
-                   getPrecedence(operatorStack[operatorTop]) >= getPrecedence(expression[index])) {
+                 expression[index] == '*' || expression[index] == '/')
+        {
+            while (operatorTop != -1 &&
+                   getPrecedence(operatorStack[operatorTop]) >= getPrecedence(expression[index]))
+            {
                 int rightOperand = popOperand();
                 int leftOperand = popOperand();
                 char operatorSymbol = popOperator();
                 int result = performOperation(leftOperand, rightOperand, operatorSymbol, errorFlag);
-                if (*errorFlag) break;
+                if (*errorFlag)
+                    break;
                 pushOperand(result);
             }
-            if (*errorFlag) break;
+            if (*errorFlag)
+                break;
             pushOperator(expression[index]);
         }
-        else {
+        else
+        {
             *errorFlag = 2;
             break;
         }
     }
 
-    if (*errorFlag == 0) {
-        while (operatorTop != -1) {   
+    if (*errorFlag == 0)
+    {
+        while (operatorTop != -1)
+        {
             int rightOperand = popOperand();
             int leftOperand = popOperand();
             char operatorSymbol = popOperator();
             int result = performOperation(leftOperand, rightOperand, operatorSymbol, errorFlag);
-            if (*errorFlag) break;
+            if (*errorFlag)
+                break;
             pushOperand(result);
         }
     }
 
-    if (*errorFlag == 0) {
+    if (*errorFlag == 0)
+    {
         finalResult = popOperand();
     }
 
     return finalResult;
 }
 
-
-int main() {
+int main()
+{
     char expression[200];
     printf("Enter expression: ");
     fgets(expression, sizeof(expression), stdin);
@@ -128,11 +156,16 @@ int main() {
     int errorFlag = 0;
     int result = evaluateExpression(expression, &errorFlag);
 
-    if (errorFlag == 1) {
+    if (errorFlag == 1)
+    {
         printf("Error: Division by zero.\n");
-    } else if (errorFlag == 2) {
+    }
+    else if (errorFlag == 2)
+    {
         printf("Error: Invalid expression.\n");
-    } else {
+    }
+    else
+    {
         printf("Result: %d\n", result);
     }
 
